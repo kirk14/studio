633ff3beef24a51958ea104370f9deac5c8028a2
Ankur Bhattacharyya <ankurbhattacharyya06@gmail.com>
2025-09-20 11:31:16 +0000

    why is it taking so much time to change from one page to another?

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,13 @@
 import type { Metadata } from 'next';
 import { Toaster } from "@/components/ui/toaster";
 import './globals.css';
+import { Space_Grotesk } from 'next/font/google';
+import { cn } from '@/lib/utils';
+
+const spaceGrotesk = Space_Grotesk({
+  subsets: ['latin'],
+  variable: '--font-space-grotesk',
+});
 
 export const metadata: Metadata = {
   title: 'GuardianHealth Pulse',
@@ -13,12 +20,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className="dark">
-      <head>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
-        <link href="https://fonts.googleapis.com/css2?family=Space+Grotesk:wght@300..700&display=swap" rel="stylesheet" />
-      </head>
+    <html lang="en" className={cn("dark", spaceGrotesk.variable)}>
+      <head />
       <body className="font-body antialiased min-h-screen">
         {children}
         <Toaster />
